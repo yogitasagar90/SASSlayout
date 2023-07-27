f71536da1fca27b3cdd9657cbdde9115b008a368
Yogita Kale <yogitasagar7557@gmail.com>
2023-07-28 04:26:12 +0530

    work on SASS layout

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const navigation = document.getElementById("navigation");
+const navToggle = document.getElementById("navToggle");
+
+
+navToggle.addEventListener("click", () => {
+    navigation.classList.toggle("active")
+})
